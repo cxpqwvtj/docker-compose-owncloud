9de8bac6013eed1c52027ed666881c5447c07319
cxpqwvtj <cxpqwvtj@gmail.com>
2016-04-17 10:18:11 +0900

    サブディレクトリで動かすように修正

--- owncloud/docker-entrypoint.sh
+++ owncloud/docker-entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
-if [ ! -e '/var/www/html/version.php' ]; then
+if [ ! -e '/var/www/html/owncloud/version.php' ]; then
+	mkdir -p /var/www/html/owncloud
+	cd /var/www/html/owncloud
 	tar cf - --one-file-system -C /usr/src/owncloud . | tar xf -
 	chown -R www-data /var/www/html
 fi
