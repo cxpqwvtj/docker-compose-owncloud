e92949835b95c2e4be4d1e6c5300e72a513103e1
cxpqwvtj <cxpqwvtj@gmail.com>
2016-04-26 16:01:06 +0900

    公式のown cloudをベースにするように修正。環境変数でベースパスを指定できるように修正

--- owncloud/docker-entrypoint.sh
+++ owncloud/docker-entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
 
-if [ ! -e '/var/www/html/owncloud/version.php' ]; then
-	mkdir -p /var/www/html/owncloud
-	cd /var/www/html/owncloud
+if [ -n "$OWNCLOUD_BASE_PATH" ]; then
+	echo "deploy at $OWNCLOUD_BASE_PATH"
+fi
+if [ ! -e '/var/www/html${OWNCLOUD_BASE_PATH}/version.php' ]; then
+	mkdir -p /var/www/html$OWNCLOUD_BASE_PATH
+	cd /var/www/html$OWNCLOUD_BASE_PATH
 	tar cf - --one-file-system -C /usr/src/owncloud . | tar xf -
-	chown -R www-data /var/www/html
+	chown -R www-data:root /var/www/html
 fi
 
 exec "$@"
